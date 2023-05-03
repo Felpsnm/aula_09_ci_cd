b3c717a5027fef9000a2ea39e21a9ef2b81cd158
Felipe Nelli Martins <Fn.martins@outlook.com>
2023-05-03 09:29:40 -0300

    Criação das funções da Calculadora com TDD

--- methods.py
+++ methods.py
@@ -5,3 +5,15 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sum(value1, value2):
+    return value1 + value2
+
+def sub(value1, value2):
+    return value1 - value2
+
+def mul(value1, value2):
+    return value1 * value2
+
+def div(value1, value2):
+    return value1 / value2
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sum():
+    # given the 2 values
+    value1 = 2
+    value2 = 5
+    
+    # when we calculate the sum
+    output = methods.sum(value1, value2)
+    
+    # then the result should be 7
+    assert output == 7
+    
+def test_sub():
+    # given the 2 values
+    value1 = 7
+    value2 = 5
+    
+    # when we calculate the subtraction
+    output = methods.sub(value1, value2)
+    
+    # then the result should be 2
+    assert output == 2
+    
+def test_mul():
+    # given the 2 values
+    value1 = 2
+    value2 = 5
+    
+    # when we calculate the multiplication
+    output = methods.mul(value1, value2)
+    
+    # then the result should be 10
+    assert output == 10
+    
+def test_div():
+    # given the 2 values
+    value1 = 4
+    value2 = 2
+    
+    # when we calculate the division
+    output = methods.div(value1, value2)
+    
+    # then the result should be 2
+    assert output == 2
